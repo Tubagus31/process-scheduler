a3112309194d27b7487e34954af863b3aa084e14
Tubagus31 <ajiprasetia79@gmail.com>
2025-11-28 23:22:26 +0700

    update useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -212,7 +212,7 @@ function App() {
   useEffect(() => {
     setAverageTime(calculateAverageWaiting(results));
     setaverageTurnAround(calculateTurnAround(results));
-  }, [processes]);
+  }, [results]);
 
   return (
     <div className="min-h-screen bg-slate-100 p-6">
